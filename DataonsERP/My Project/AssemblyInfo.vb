970b2e203c7ad480f1cb3895d99581dcfbdbdec6
Ercan Özyanar <ercanozyanar@gmail.com>
2021-11-16 12:30:53 +0300

    Proje dosyası ekle.

---
+++ DataonsERP/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+' denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+' bu öznitelik değerlerini değiştirin.
+
+' Bütünleştirilmiş kod özniteliklerinin değerlerini gözden geçirin
+
+<Assembly: AssemblyTitle("DataonsERP")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DataonsERP")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+<Assembly: Guid("b38ed2af-667a-4ce0-b895-ed3e2b24bd56")>
+
+' Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+'
+'      Ana Sürüm
+'      İkincil Sürüm 
+'      Yapı Numarası
+'      Düzeltme
+'
+' Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+' '*' kullanarak varsayılana ayarlayabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
