80f7b1f24be335f4c5669691a8ee488c6bb857d1
Ercan Özyanar <ercanozyanar@gmail.com>
2021-11-16 12:36:51 +0300

    Start

--- DataonsERP/main.Designer.vb
+++ DataonsERP/main.Designer.vb
@@ -1,6 +1,6 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
-    Inherits System.Windows.Forms.Form
+Partial Class main
+    Inherits DevExpress.XtraEditors.XtraForm
 
     'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
     <System.Diagnostics.DebuggerNonUserCode()> _
@@ -22,10 +22,20 @@ Partial Class Form1
     'Kod düzenleyicisini kullanarak değiştirmeyin.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.FormAssistant1 = New DevExpress.XtraBars.FormAssistant()
+        Me.SuspendLayout()
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(945, 520)
+        Me.IconOptions.SvgImage = Global.DataonsERP.My.Resources.Resources.encrypt
+        Me.Name = "main"
+        Me.Text = "Dataons ERP"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents FormAssistant1 As DevExpress.XtraBars.FormAssistant
 End Class
